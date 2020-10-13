b36bd0fcc214b1e81869d533a16159b5acab2856
HeitorTerra <72770853+HeitorTerra@users.noreply.github.com>
2020-10-12 21:01:01 -0300

    Add files via upload

---
+++ FuzzyCMeans_Comentado.py
@@ -0,0 +1,61 @@
+# Importando as bibliotecas
+from sklearn.datasets import load_wine # Dataset de vinhos
+import skfuzzy as fuzz # Biblioteca skfuzzy contendo o algoritmo Fuzzy C-Means
+import pandas as pd # Biblioteca do pandas (para tratar o dataset)
+import numpy as np # Biblioteca do numpy (para operar com matrizes)
+import matplotlib.pyplot as plt # Biblioteca do matplotlib.pyplot (para plotar gráficos)
+
+# Carregando o dataset
+wine = load_wine() # A variável "wine" recebe o dataset de vinhos
+wine_df = pd.DataFrame(data= np.c_[wine['data'], wine['target']], columns= wine['feature_names'] + ['target']) # Transformando o dataset em um DataFrame do pandas
+
+# Analisando o dataset
+"""
+wine_df.describe() # A função describe() abre uma tabela com informações de todas as colunas do dataset
+wine_df_0 = wine_df.iloc[:59] # Parte do dataset contendo somente vinhos do tipo 0
+wine_df_1 = wine_df.iloc[59:130] # Parte do dataset contendo somente vinhos do tipo 1
+wine_df_2 = wine_df.iloc[130:] # Parte do dataset contendo somente vinhos do tipo 2
+wine_df_0.head(10) # A função head() abre uma tabela de visualização do dataset
+wine_df_1.head(10) # O número 10 significa que 10 linhas serão mostradas na tabela
+wine_df_2.head(10) # As três linhas de código com o comando "head()" abrem as tabelas de cada tipo de vinho
+wine_df.corr() # A função corr() mostra a correlação das "features"(Colunas) com o "target"(Resposta)
+"""
+# Tratando o dataset
+wine_shuffle = wine_df.sample(frac=1).reset_index(drop=True) # A função sample() embaralha o dataset, frac = 1 garante que nenhum dado se perca, e a função reset_index() reorganiza os valores dos indexadores das linhas
+wine_shuffle.head(20) # Printando o dataset para conferir se o código acima funcionou
+
+# Dividindo o dataset
+Features = ['malic_acid', 'total_phenols', 'flavanoids', 'od280/od315_of_diluted_wines'] # Armazenando as colunas que serão usadas no treino na variável "Features"
+x_train, x_test = wine_shuffle[Features][:130], wine_shuffle[Features][130:] # Separando 130 linhas de dados do dataset para o dataset de treino (x_train) e o restante para teste (x_test)
+y_test = wine_shuffle['target'][130:] # Fazendo o mesmo que no código acima, porém para a coluna "target", das respostas
+x_test.head() # Conferindo o código acima
+
+#Implementa o Algoritmo Fuzzy C-means
+cntr, u_train, u0_train, d_train, jm_train, p_train, fpc_train = fuzz.cluster.cmeans(
+    x_train.transpose(), 3, 3, error=0.005, maxiter=1000, init=None) # Treinando o algoritmo
+
+# Testando o algoritmo
+u_test, u0_test, d_test, jm_test, p_test, fpc_test = fuzz.cluster.cmeans_predict(
+    x_test.transpose(), cntr, 2, error=0.005, maxiter=1000) # Prevendo os novos valores
+prediction = np.argmax(u_test, axis=0) # Salvando a matriz dos resultados obtidos na variável "prediction"
+
+# Calculando a precisão do modelo
+acertos = 0 # Variável para contar a quantidade de respostas corretas
+for i in range(len(y_test)):
+    if prediction[i] == y_test[i+130]: # Comparando os resultados previstos com os resultados reais
+        acertos += 1 # Somando os resultados corretos
+accuracy = (acertos/len(y_test)) # Calculando a precisão do modelo
+print('Precisão do modelo: {}'.format(accuracy)) # Imprimindo a precisão do modelo
+
+# Plotando os resultados
+pontos = [] # Lista vazia para armazenar os pontos 
+for i in range(len(y_test)):
+    pontos.append(i) # Adicionando os pontos na lista
+plt.figure() # Criando a figura
+plt.title('Resultados Obtidos vs Resultados Reais (Fuzzy C-Means)') # Título do gráfico
+plt.scatter(pontos, prediction, label='Resultados Obtidos') # Plotando os resultados previstos
+plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.25)) # Configurando a legenda
+plt.scatter(pontos, y_test.reset_index(drop=True), label='Resultados Reais') # Plotando os resultados reais
+plt.legend(loc='best', bbox_to_anchor=(0.5, 0., 0.5, 0.25)) # Configurando a legenda
+plt.xlabel('Amostragem') # Nome da abscissa
+plt.ylabel('Respostas') # Nome da ordenada
